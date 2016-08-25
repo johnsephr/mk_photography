bd113dccb99de351b30beb9318bdd8f338ee0f57
John <John@Users-MacBook-Pro-2.local>
2016-08-25 17:37:17 -0400

    First commit for mk_photography

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mkPhotographyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mkPhotographyApp
+ */
+angular.module('mkPhotographyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mkPhotographyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mkPhotographyApp
+ */
+angular.module('mkPhotographyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
